0376ce39845408e7466fecde24bb45ea3593209c
Jason Niles <jason@gravitylens.net>
2025-09-22 12:40:07 -0500

    Enhance Samba user management in entrypoint.sh and update environment variables in docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,12 @@ git clone --depth=1 "${GIT_REPO_URL}" /tmp/gitclone
 rm -rf /srv/gitrepo/*
 cp -r /tmp/gitclone/* /srv/gitrepo/
 
+# Create a Samba user (using environment variables for flexibility)
+if [[ -n "${SAMBA_USER}" && -n "${SAMBA_PASS}" ]]; then
+    useradd -M -s /sbin/nologin "${SAMBA_USER}"
+    echo -e "${SAMBA_PASS}\n${SAMBA_PASS}" | smbpasswd -a -s "${SAMBA_USER}"
+fi
+
 # Create samba config file
 cat >/etc/samba/smb.conf <<EOF
 [global]
@@ -18,7 +24,8 @@ cat >/etc/samba/smb.conf <<EOF
 [share]
    path = /srv/gitrepo
    read only = yes
-   guest ok = yes
+   guest ok = no
+   valid users = ${SAMBA_USER}
 EOF
 
 # Start Samba services (daemon mode)
